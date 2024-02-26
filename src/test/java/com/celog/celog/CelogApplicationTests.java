f56ec2e6d7bea40b497e3a44ae17445f99c3d75c
heihyun <jeong_hei_hyun@naver.com>
2024-02-26 23:21:43 +0900

    Initial commit

---
+++ src/test/java/com/celog/celog/CelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celog.celog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
