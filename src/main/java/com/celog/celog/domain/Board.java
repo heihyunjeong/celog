239d0d65aedd2ddf881a636b99271c2af3025486
smilejakdu <ash982416@gmail.com>
2024-03-13 08:47:45 +0900

    update board entity

--- src/main/java/com/celog/celog/domain/Board.java
+++ src/main/java/com/celog/celog/domain/Board.java
@@ -21,11 +21,11 @@ public class Board {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column()
+    @Column(nullable = false)
     private String title;
 
-    @Column()
-    private Long content;
+    @Column(nullable = false)
+    private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
